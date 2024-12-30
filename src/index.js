82527d0c4d795b1537f527e25e602e30656b4286
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-12-30 10:45:52 -0800

    initial function created and is alerting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-alert("Hello there");
+function generatePoem(event) {
+  event.preventDefault();
+
+  alert("Generateing poem");
+}
+let poemFormElement = document.querySelector("#search-form");
+poemFormElement.addEventListener("submit", generatePoem);

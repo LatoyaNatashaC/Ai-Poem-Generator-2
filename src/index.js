72b0551d19bf7ba5689fb1f5935ca1bd06365a27
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-12-31 14:09:20 -0800

    added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 function generatePoem(event) {
   event.preventDefault();
 
-  let poemElement = document.querySelector("#poem-location");
-  poemElement.innerHTML = "about the rain, the fog, the dew";
+  new Typewriter("#poem-location", {
+    strings:
+      "A tree is beautiful, but what's more, it has a right to life; like water, the sun, and the stars, it is essential. Life on earth is inconceivable without trees.",
+    autoStart: true,
+    delay: 1.5,
+    cursor: "",
+  });
 }
 let poemFormElement = document.querySelector("#search-form");
 poemFormElement.addEventListener("submit", generatePoem);

f9cfc503bcff153f6c747576eb11b522aaecde3a
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-12-31 20:10:54 -0800

    added api key and url

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 function generatePoem(event) {
   event.preventDefault();
 
+  let apiKey = "4f3b0tf3219b4c7758082d0o48eabbbe";
+  let prompt = "";
+  let context = "";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
   new Typewriter("#poem-location", {
     strings:
       "A tree is beautiful, but what's more, it has a right to life; like water, the sun, and the stars, it is essential. Life on earth is inconceivable without trees.",

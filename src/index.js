6518de6c722c2599f5ab332577317de83474c233
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2025-01-01 10:46:46 -0800

    added ai api integration

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let apiKey = "4f3b0tf3219b4c7758082d0o48eabbbe";
-  let prompt = "";
-  let context = "";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
-
+function displayPoem(response) {
   new Typewriter("#poem-location", {
-    strings:
-      "A tree is beautiful, but what's more, it has a right to life; like water, the sun, and the stars, it is essential. Life on earth is inconceivable without trees.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1.5,
     cursor: "",
   });
+}
+function generatePoem(event) {
+  event.preventDefault();
+  let userInput = document.querySelector("#user-input");
+  let apiKey = "4f3b0tf3219b4c7758082d0o48eabbbe";
+  let prompt = `User instructions: generate a poem about ${userInput.value}`;
+  let context =
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a four line poem in basic html. Do not show the words html. Follow the user instructions";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(`prompt:${prompt}`);
+  console.log(`context:${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#search-form");
 poemFormElement.addEventListener("submit", generatePoem);
